1e498bc246a62a5a3972962d5c8875187d1dde24
Sarthak Pandey <pandeysarthak06@gmail.com>
2023-10-17 11:57:15 +0530

    Initial commit

---
+++ videoapp/urls.py
@@ -0,0 +1,14 @@
+from . import views
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('register/', views.register, name = 'register'),
+    path('login/', views.login_view, name = 'login'),
+    path('dashboard/', views.dashboard, name = 'dashboard'),
+    path('meeting/', views.videocall, name = 'meeting'),
+    path('logout/', views.logout_view, name = 'logout'),
+    path('join/', views.join_room, name = 'join'),
+    path('',views.index, name='index'),
+
+]
